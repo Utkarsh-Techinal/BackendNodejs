174f9719c578d9a27389c813af4ede3c76f5be8c
Utkarsh <1998utkarshshukla@gmail.com>
2021-01-02 15:48:19 +0530

    Express Internmidiate

---
+++ 04ExpressIntermidiate/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+var myconsolelog = function(req,res,next){
+    console.log("I am Middleware");
+    next();
+};
+
+app.get("/" , (req,res)=>{
+    res.send("Hello World!");
+});
+
+app.listen(3000, ()=> console.log("Server is running at port 3000...."));
